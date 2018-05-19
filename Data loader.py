da383c4cb1def9d9c9d12b68794c73f053e79a99
Rui Cao <ruc074@eng.ucsd.edu>
2018-05-20 07:49:16 +0800

    Update Data loader.py

--- Data loader.py
+++ Data loader.py
@@ -7,8 +7,25 @@ import torchvision.transforms as transforms
 
 # Define a data loader
 
-def data_loader(index, root, mode, Transform_data, Transform_lbl):
+def data_loader(index, root, mode, Transform_data, Transform_lbl, ctg_index):
+
+    def Categories(IMG_path, input_list):
     
+    
+        input_image = Image.open(IMG_path)
+        input_list.sort()
+
+
+        C = len(input_list)
+        target = np.zeros((input_image.shape[0], input_image.shape[1]))
+
+        for i in range(C):
+            target[np.where(input_image == input_list[i])] = i
+        target = np.array(target)
+        
+        return target
+
+
     image_path0 = root + mode + '/' + mode + '_'
     
     for idx in index:
@@ -18,6 +35,10 @@ def data_loader(index, root, mode, Transform_data, Transform_lbl):
         lbl_path  = image_path0 + '{0:05d}'.format(idx) + '_lbl.png'
         data_idx = Image.open(data_path)
         lbl_idx  = Image.open(lbl_path)
+
+
+        # Calculate categories of label image
+        lbl_idx  =  Categories(lbl_idx, ctg_index)
         
         # Apply Resize, ToTensor, Normalization
         data_out = Transform_data(data_idx)
